0bdb7851a0d7a16eeb6c2e09f8d01ff88bfd225f
ardhi <gedeardhiyana@gmail.com>
2024-11-26 10:20:31 +0800

    Add user login, improve user update and create

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,6 +46,72 @@ exports.createUser = async (request, h) => {
   }
 };
 
+//hapus user
+exports.deleteUser = async (request, h) => {
+  try {
+    const result = await userModel.deleteUser(request.params.id);
+    if (result[0].affectedRows === 0) {
+      return h.response({ message: 'user not found' }).code(404);
+    }
+    return h.response({ message: 'user delete successfully' }).code(200);
+  } catch (error) {
+    return h.response({ error: error.message }).code(500);
+  }
+};
+
+//login user
+const secretKey = 'dfvbjiuyu54598901!@#%R&V^*)';
+
+exports.login = async (request, h) => {
+  try {
+    const { email, password } = request.payload;
+
+    //validasi input
+    if (!email || !password) {
+      return h.response({ error: 'email and password are required' }).code(400);
+    }
+
+    //cari user berdasarkan email
+    const user = await userModel.getUsersByEmail(email);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return h.response({ error: 'invalid email or password' }).code(401);
+    }
+
+    //buat token jwt
+    const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+
+    return h
+      .response({
+        message: 'login successfully',
+        token,
+        user: { id: user.id, email: user.email, nama: user.nama },
+      })
+      .code(200);
+  } catch (error) {
+    return h.response({ error: error.message }).code(500);
+  }
+};
+
+exports.verifyToken = async (request, h) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return h.response({ error: 'token not provided' }).code(401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    console.log('Decoded token:', decoded);
+    request.user = decoded;
+    return h.continue;
+  } catch (error) {
+    console.error('token verification error', err.message);
+    return h.response({ error: 'invalid token' }).code(401);
+  }
+};
+
 //update data user
 exports.updateUser = async (request, h) => {
   try {
@@ -56,65 +122,23 @@ exports.updateUser = async (request, h) => {
       return h.response({ error: 'All fields are required: nama, email, password' }).code(400);
     }
 
-    const result = await userModel.updateUser({ nama, email, password }, id);
+    let hashedPassword = undefined;
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    const updateuser = {
+      nama: nama || undefined,
+      email: email || undefined,
+      password: hashedPassword || undefined,
+    };
+
+    const result = await userModel.updateUser(updateuser, id);
 
     if (result[0].affectedRows === 0) {
       return h.response({ message: 'user not found' }).code(404);
     }
     return h.response({ message: 'user update successfully' }).code(200);
-  } catch (error) {
-    return h.response({ error: error.message }).code(500);
-  }
-};
-
-//hapus user
-exports.deleteUser = async (request, h) => {
-  try {
-    const result = await userModel.deleteUser(request.params.id);
-    if (result[0].affectedRows === 0) {
-      return h.response({ message: 'user not found' }).code(404);
-    }
-    return h.response({ message: 'user update successfully' }).code(200);
-  } catch (error) {
-    return h.response({ error: error.message }).code(500);
-  }
-};
-
-//login user
-exports.login = async (request, h) => {
-  try {
-    const { email, password } = request.payload;
-    console.log('Email:', email);
-    console.log('Password:', password);
-
-    //validasi input
-    if (!email || !password) {
-      return h.response({ error: 'email and password are required' }).code(400);
-    }
-
-    //cari user berdasarkan email
-    const user = await userModel.getUsersByEmail(email);
-    if (!user) {
-      return h.response({ error: 'invalid email or password' }).code(401);
-    }
-
-    //periksa password
-    const passwordValid = await bcrypt.compare(password, user.password);
-    console.log('Password valid:', passwordValid);
-    if (!passwordValid) {
-      return h.response({ error: 'invalid email or password' }).code(401);
-    }
-
-    //buat token jwt
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    return h
-      .response({
-        message: 'login successfully',
-        token,
-        user: { id: user.id, email: user.email, nama: user.nama },
-      })
-      .code(200);
   } catch (error) {
     return h.response({ error: error.message }).code(500);
   }
