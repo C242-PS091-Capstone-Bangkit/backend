c8541f355e6c95af9c7200fc22640d29d0f4054b
ardhi024 <114859752+ardhi024@users.noreply.github.com>
2024-12-13 12:28:53 +0800

    Update userRoutes.js
    fix api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,17 @@
-const { options } = require('@hapi/joi/lib/base');
 const userController = require('../controllers/userController');
 
 const routes = [
   { method: 'GET', path: '/users', handler: userController.getAllUsers },
   { method: 'GET', path: '/users/{id}', handler: userController.getUsersById },
   { method: 'POST', path: '/users', handler: userController.createUser },
+  { method: 'PUT', path: '/users/{id}', handler: userController.updateUser },
   {
     method: 'PUT',
-    path: '/users/{id}',
+    path: '/users/{id}/password',
     options: {
       pre: [userController.verifyToken],
     },
-    handler: userController.updateUser,
+    handler: userController.updatePasswordUser,
   },
   { method: 'DELETE', path: '/users/{id}', handler: userController.deleteUser },
   { method: 'POST', path: '/login', handler: userController.login },
