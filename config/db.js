d35a4d78fef681fb09e3ca67925532d9dac0a8c1
ardhi024 <114859752+ardhi024@users.noreply.github.com>
2024-12-13 12:17:48 +0800

    Update db.js
    fix api

--- config/db.js
+++ config/db.js
@@ -8,9 +8,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true, // Menunggu koneksi jika pool penuh
-  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10, // Batas koneksi dalam pool
-  queueLimit: 0, // Tidak membatasi antrean koneksi
+  waitForConnections: true,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  queueLimit: 0,
 });
 
 module.exports = pool.promise();
