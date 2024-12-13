8828c2faf13b29597c927d7a3c8f8baf1cc32a05
ardhi024 <114859752+ardhi024@users.noreply.github.com>
2024-12-13 12:28:05 +0800

    Update reminderRoutes.js

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -1,11 +1,11 @@
 const reminderController = require('../controllers/reminderController');
 
 const reminderRoutes = [
-  { method: 'POST', path: '/reminder', handler: reminderController.createReminder },
-  { method: 'GET', path: '/reminder', handler: reminderController.getAllReminder },
-  { method: 'GET', path: '/reminder/{id}', handler: reminderController.getReminderById },
-  { method: 'PUT', path: '/reminder/{id}', handler: reminderController.updateReminder },
-  { method: 'DELETE', path: '/reminder/{id}', handler: reminderController.deleteReminder },
+  { method: 'POST', path: '/reminders', handler: reminderController.createReminder },
+  { method: 'GET', path: '/reminders', handler: reminderController.getAllReminder },
+  { method: 'GET', path: '/reminders/{id}', handler: reminderController.getReminderById },
+  { method: 'PUT', path: '/reminders/{id}', handler: reminderController.updateReminder },
+  { method: 'DELETE', path: '/reminders/{id}', handler: reminderController.deleteReminder },
 ];
 
 module.exports = reminderRoutes;
