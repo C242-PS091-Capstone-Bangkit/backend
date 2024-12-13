a3101b404408b08b619ae472cac825b7d9813bb4
ardhi024 <114859752+ardhi024@users.noreply.github.com>
2024-12-13 12:26:41 +0800

    Update reminderModel.js
    fix api

--- models/reminderModel.js
+++ models/reminderModel.js
@@ -1,14 +1,17 @@
 const db = require('../config/db');
 
-const getAllReminders = () => db.query('SELECT * FROM reminder ORDER BY jam_reminder ASC');
-const getReminderById = (id) => db.query('SELECT * FROM reminder WHERE id_reminder = ?', [id]);
+const getAllReminders = async () => {
+  const [rows] = await db.query('SELECT * FROM reminders');
+  return rows;
+};
+const getReminderById = (id) => db.query('SELECT * FROM reminders WHERE id_reminder = ?', [id]);
 const createReminder = (reminder) => {
   const { id_user, judul_reminder, deskripsi, jam_reminder } = reminder;
-  const query = 'INSERT INTO reminder (id_user, judul_reminder, deskripsi, jam_reminder) VALUES (?, ?, ?, ?)';
+  const query = 'INSERT INTO reminders (id_user, judul_reminder, deskripsi, jam_reminder) VALUES (?, ?, ?, ?)';
 
   return db.query(query, [id_user, judul_reminder, deskripsi, jam_reminder]);
 };
-const updateReminder = (reminder, id) => db.query('UPDATE reminder SET judul_reminder = ?, deskripsi = ?, jam_reminder = ? WHERE id_reminder = ?', [reminder.judul_reminder, reminder.deskripsi, reminder.jam_reminder, id]);
-const deleteReminder = (id) => db.query('DELETE FROM reminder WHERE id_reminder = ?', [id]);
+const updateReminder = (reminder, id) => db.query('UPDATE reminders SET judul_reminder = ?, deskripsi = ?, jam_reminder = ? WHERE id_reminder = ?', [reminder.judul_reminder, reminder.deskripsi, reminder.jam_reminder, id]);
+const deleteReminder = (id) => db.query('DELETE FROM reminders WHERE id_reminder = ?', [id]);
 
 module.exports = { getAllReminders, getReminderById, createReminder, updateReminder, deleteReminder };
